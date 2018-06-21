a5b102cf532fabe99dffa3c25a5111a55c2d8c2e
chm <mane360@yahoo.com>
2018-06-21 20:46:10 +1200

    Add file reading test

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ INDEX_EXTRA_DATA = 6
 
 def fill_networks(seed, filename):
     f = open(filename)
+    results = []   
+    
+    f.readline() # skip header
+    
     for line in f:
         list_line = line.split("\t")
         g1 = list_line[INDEX_G1]
@@ -12,9 +16,24 @@ def fill_networks(seed, filename):
         score = list_line[INDEX_SCORE]
         extra = list_line[INDEX_EXTRA_DATA:]
         
-        print("g1: %s, g2: %s, score: %s, extra: %s" % (g1, g2, score, extra))
-        break
+        # print("g1: %s, g2: %s, score: %s, extra: %s" % (g1, g2, score, extra))
+        
+        to_return = [g1, g2, score]
+        to_return.extend(extra)
+        results.append(to_return)    
     
     f.close()
+    return results
 
-fill_networks(1, "test_data")
+# TESTS
+
+def test_readfile_and_output():
+    got = fill_networks(1, "test_readfile_and_output")
+    target = [["1", "2", "3", "4", "5", "6", "7", "8\n"]]
+    if (got != target):
+        print("FAIL!\ngot: %s\ntarget: %s\n" % (got, target))
+    else:
+        print("pass")
+          
+
+test_readfile_and_output()
