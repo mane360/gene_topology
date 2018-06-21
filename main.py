f3b91818b8a2fdc09808bb42355bd0f22c295a17
chm <mane360@yahoo.com>
2018-06-21 21:02:20 +1200

    Test: two rows, one in seed

--- main.py
+++ main.py
@@ -5,9 +5,13 @@ INDEX_EXTRA_DATA = 6
 
 def fill_networks(seed, filename):
     f = open(filename)
-    results = []   
-    
+    results = []       
     f.readline() # skip header
+    
+    
+    # initialise empty set of sets
+    # each set represents a connected network
+    networks = set()    
     
     for line in f:
         list_line = line.split("\t")
@@ -16,11 +20,12 @@ def fill_networks(seed, filename):
         score = list_line[INDEX_SCORE]
         extra = list_line[INDEX_EXTRA_DATA:]
         
-        # print("g1: %s, g2: %s, score: %s, extra: %s" % (g1, g2, score, extra))
+        if (g1 in seed or g2 in seed): # todo parse score
+            networks.update(set([g1, g2]))
         
-        to_return = [g1, g2, score]
-        to_return.extend(extra)
-        results.append(to_return)    
+            row = [g1, g2, score]
+            row.extend(extra)
+            results.append(row)    
     
     f.close()
     return results
@@ -28,12 +33,21 @@ def fill_networks(seed, filename):
 # TESTS
 
 def test_readfile_and_output():
-    got = fill_networks(1, "test_readfile_and_output")
+    got = fill_networks(["1"], "test_readfile_and_output")
     target = [["1", "2", "3", "4", "5", "6", "7", "8\n"]]
     if (got != target):
         print("FAIL!\ngot: %s\ntarget: %s\n" % (got, target))
     else:
         print("pass")
           
+def test_two_rows_one_with_seed():
+    got = fill_networks(["1"], "test_two_rows_one_with_seed")
+    target = [["1", "2", "1.0", "0", "0", "0", "0", "0\n"]]
+    if (got != target):
+        print("FAIL!\ngot: %s\ntarget: %s\n" % (got, target))
+    else:
+        print("pass")
+
 
 test_readfile_and_output()
+test_two_rows_one_with_seed()
